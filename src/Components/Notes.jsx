4ebcf4d2fd63a9183d76f43fad919d0a8fbf3ebb
AshishBhardwaj1 <fireash70@gmail.com>
2023-11-27 12:09:44 +0530

    another commit

--- src/Components/Notes.jsx
+++ src/Components/Notes.jsx
@@ -1,39 +1,76 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import "./notes.css"
 
+const getItems =()=>{
+  let note = localStorage.getItem('note')
+  console.log(note);
+  if(note){
+    return JSON.parse(localStorage.getItem('note'))
+  } else{
+    return []
+  }
+}
 let id=0;
 const Notes = () => {
   const [input,setInput] =useState('')
-  const [item ,setItem] =useState([])
+  const [item ,setItem] =useState(getItems())
+  const [search,setSearch] =useState("")
+ useEffect(()=>{
+localStorage.setItem("note",JSON.stringify(item))
+ },[item])
   const addNote =()=>{
-  setItem( [ ...item,{
-      id:id++,
-      input:input
-    }])
-    
-    
+    if (input.trim()!=="") {
+
+      setItem( [ ...item,{
+          id:id++,
+          input:input,
+          
+        }])
+        setInput('')
+    }
   }
+  const editTodo =(item)=>{
+            item.filter(a=>a.id)
+            setItem(input)
+  }
+  // const searchedItem = item.filter(items=> items.toLowerCase().includes(search.toLowerCase())
+  // )
   return (
     <div className='notes'>
       <h1>Notes taking App</h1>
       <div>
 
-<input type="text" placeholder='add your notes here...'
+<input type="text" value={input} placeholder='add your notes here...' 
  onChange={(e)=>setInput(e.target.value)}
 
 />
-<button onClick={addNote}>add note</button>
+<button className='btn' onClick={addNote}>add note</button>
       </div>
-<div className='card-container'>
+      
+<div className='card-container' >
+  <div className='search'> 
+
+<input type="search" placeholder='search your notes here' value={search} onChange={(e)=>setSearch(e.target.value)} />
+  </div>
+  <div style={{display:"flex" , flexWrap:"wrap"}}> 
+
 {
   item.map((prod,id)=>(
     <div className='card'>
-    <li key={id}>{prod.input} {' '}
-     <button className='del' onClick={()=>setItem(item.filter(a=> a.id !== prod.id))}>delete note</button>
+      <div className="text" >
+
+    <li key={id} >{prod.input}  {' '}
      </li>
+      </div>
+    <div style={{position:"absolute",bottom:"10px"}}>
+
+     <button className='card-btn'  onClick={()=>setItem(item.filter(a=> a.id !== prod.id))}>delete</button>
+     <button className='card-btn' onClick={editTodo}>edit</button>
+    </div>
     </div>
   ))
 }
+  </div>
   </div>
     </div>
     
